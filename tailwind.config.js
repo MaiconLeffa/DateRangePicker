620bea9a9c01db32eb75b06c113610e95e6bb47e
Maicon <maicon.leffa@eumedicoresidente.com.br>
2024-03-07 20:15:15 -0300

    feat: add just in time mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  mode: "jit",
   content: [
     "./src/stories/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
